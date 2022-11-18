054dcc044f5d07b564815419d013d7ccd587171d
Hugo Queinnec <67365803+hugoqnc@users.noreply.github.com>
2022-11-18 17:38:13 +0100

    ./switch.sh to easily switch between cpu files

---
+++ samples/simple/switch.sh
@@ -0,0 +1,18 @@
+FILE_EXEC=../../dace/codegen/targets/cpu.py
+FILE_TASKING=../../dace/codegen/targets/cpu_tasking.py
+FILE_FOR=../../dace/codegen/targets/cpu_save.py
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+if test -f "$FILE_FOR"; then
+    # exec file contains tasking implementation
+    mv $FILE_EXEC $FILE_TASKING;
+    mv $FILE_FOR $FILE_EXEC;
+    echo -e "$FILE_EXEC now contains ${RED}parallel for${NC} implementation";
+else
+    # exec file contains parallel for implementation
+    mv $FILE_EXEC $FILE_FOR;
+    mv $FILE_TASKING $FILE_EXEC;
+    echo "$FILE_EXEC now contains ${RED}tasking${NC} implementation";
+fi
