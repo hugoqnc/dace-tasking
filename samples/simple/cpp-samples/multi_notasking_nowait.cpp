c1972db71ecb7582498475a6d34f9f81d0bacc4d
Hugo Queinnec <67365803+hugoqnc@users.noreply.github.com>
2022-12-10 15:23:55 +0100

    add nowait example

--- samples/simple/cpp-samples/multi_notasking_nowait.cpp
+++ samples/simple/cpp-samples/multi_notasking_nowait.cpp
@@ -8,38 +8,33 @@ int main(){
     int iterationsPerLoop = 100;
     omp_set_nested(1);
 
+    #pragma omp parallel
     {
-        #pragma omp parallel for
+        #pragma omp for nowait
         for (auto i = 0; i < iterationsPerLoop; i += 1) {
             {
                 std::this_thread::sleep_for(std::chrono::milliseconds(sleepMillis));
                 printf("\u001b[3%dm1:%d[%d] \u001b[30m",omp_get_thread_num(),i,omp_get_thread_num());
             }
         }
-    }
-    printf("\n\n");
-    {
-        #pragma omp parallel for
+
+        #pragma omp for nowait
         for (auto i = 0; i < iterationsPerLoop; i += 1) {
             {
                 std::this_thread::sleep_for(std::chrono::milliseconds(sleepMillis));
                 printf("\u001b[3%dm2:%d[%d] \u001b[30m",omp_get_thread_num(),i,omp_get_thread_num());
             }
         }
-    }
-    printf("\n\n");
-    {
-        #pragma omp parallel for
+
+        #pragma omp for nowait
         for (auto i = 0; i < iterationsPerLoop; i += 1) {
             {
                 std::this_thread::sleep_for(std::chrono::milliseconds(sleepMillis));
                 printf("\u001b[3%dm3:%d[%d] \u001b[30m",omp_get_thread_num(),i,omp_get_thread_num());
             }
         }
-    }
-    printf("\n\n");
-    {
-        #pragma omp parallel for
+
+        #pragma omp for nowait
         for (auto i = 0; i < iterationsPerLoop; i += 1) {
             {
                 std::this_thread::sleep_for(std::chrono::milliseconds(sleepMillis));
